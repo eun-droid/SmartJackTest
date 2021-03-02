4a8240f9f37e6bcf0019538bbc5ca0a314b4e174
eunsub kim <deveunsub@gmail.com>
2021-03-02 22:51:42 +0900

    잘못 쓴 주석 수정

--- app/src/main/java/com/yes/smartjacktest/extensions/RxJavaExtensions.kt
+++ app/src/main/java/com/yes/smartjacktest/extensions/RxJavaExtensions.kt
@@ -3,7 +3,7 @@ package com.yes.smartjacktest.extensions
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
-// CompositeDisposable의 '+=' 연산자 뒤에 Disposable 타입이 오는 경우를 재정의함
+// CompositeDisposable의 '+=' 연산자 뒤에 Disposable 타입이 오는 경우를 오버로딩함
 operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
     this.add(disposable)
 }
